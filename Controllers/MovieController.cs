f7fd37e7b48a24e340b59b808fb7a1b65b88a7ea
Arnonthawajjana Chainarong <Arnonthawajjana@hotmail.com>
2019-06-11 21:24:16 +0700

    test test test

---
+++ Controllers/MovieController.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using SanookMovie.Models;
+
+namespace SanookMovie.Controllers
+{
+    public class MovieController : Controller
+    {
+        static IList<Movie> _movies = new List<Movie>();
+        public MovieController()
+        {
+        }
+        public IActionResult Index()
+        {
+            return View(_movies);
+        }
+        public IActionResult Create()
+        {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult Create(Movie movie)
+        {
+            _movies.Add(movie);
+            return RedirectToAction(nameof(Index));
+        }
+        public IActionResult Delete(int id)
+        {
+            var movie = _movies.FirstOrDefault(x => x.Id==id);
+            if(movie == null)return NotFound();
+            return View(movie);
+        }
+        public IActionResult DeleteConfirm(int id)
+        {
+            var movie =_movies.FirstOrDefault(x => x.Id==id);
+            if(movie == null)return NotFound();
+            _movies.Remove(movie);
+            return RedirectToAction(nameof(Index));
+        }
+        public IActionResult Details()
+        {
+            return View();
+        }
+        public IActionResult Edit()
+        {
+            return View();
+        }
+        
+    }
+}
